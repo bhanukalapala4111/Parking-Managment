bfb7397d49a1f7bfc7e60e2403213792208d5dcb
Bhanu Kalapala <Bhanuprasad4111@gmail.com>
2026-01-29 14:24:41 +0530

    Adding project to rep

---
+++ src/main/java/com/Practice/Parking/Managment/ParkingManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.Practice.Parking.Managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practice/Parking/Managment/ParkingManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practice.Parking.Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
